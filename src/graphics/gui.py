24ce2eff4d52ac0a47fcd6043fef05ac97199e8c
Justin <jcrouch938@gmail.com>
2019-04-30 17:14:39 -0500

    Added timer function

--- src/graphics/gui.py
+++ src/graphics/gui.py
@@ -4,6 +4,7 @@
 # Changes: Fixed the comments for setting up the different GUIs.
 #####################################################################
 from Tkinter import *
+from time import time, sleep
 
 #----[CLASSES]-------------------------------------------------------
 # Game Class
@@ -18,6 +19,8 @@ class Game(Frame):
 		self.pack(fill=BOTH, expand=1)
 
 		Game.display = Frame(self)
+
+		self.events = []
 
 	# Setup GUI for main screen
 	def setupHomeScreen(self):
@@ -55,7 +58,7 @@ class Game(Frame):
 		help = Button(Game.display, activebackground="#AAA", bg="#CCC", height=1, width=9, padx=5, text="Help", command=lambda:self.loadScreen(self.setupHelp))
 		help.place(width=button_width, height=button_height, x=about_x, y=about_y+button_height+button_spacing)
 
-		exitB = Button(Game.display, activebackground="#AAA", bg="#CCC", fg="#F22", height=1, width=9, padx=5, text="Exit", command=lambda:exit(0))
+		exitB = Button(Game.display, activebackground="#AAA", bg="#CCC", fg="#F22", height=1, width=9, padx=5, text="Exit", command=lambda:self.loadScreen("EXIT"))
 		exitB.place(width=button_width, height=button_height, x=about_x, y=about_y+(button_height+button_spacing)*2)
 
 	# Setup GUI for easy mode
@@ -71,6 +74,12 @@ class Game(Frame):
 		# Create back button
 		back = Button(Game.display, activebackground="#AAA", bg="#CCC", fg="#F22", height=1, width=9, padx=5, text="Back", command=lambda:self.loadScreen(self.setupHomeScreen))
 		back.place(width=100, height=40, x=0, y=0)
+
+		# Create timer
+		timer = Timer(Game.display, 60)
+		timer.start()
+
+		self.events.append(lambda:timer.update())
 
 	# Setup GUI for medium mode
 	def setupMediumMode(self):
@@ -148,15 +157,54 @@ class Game(Frame):
 
 	# Clean the display, update the display, display the display
 	def loadScreen(self, setupFunc):
+		global alive
+
+		if(setupFunc == "EXIT"):
+			alive = False
+			return
+
 		Game.display.destroy()
+		self.events = [lambda:""]
 		setupFunc()
 
 		Game.display.pack(fill=BOTH, expand=1)
+
+	def handleEvents(self):
+		for event in self.events:
+			event()
+
+
+class Timer(Text):
+
+	# Duration for the timer to count from, in seconds
+	def __init__(self, master, duration):
+		Text.__init__(self, master, height=1, width=6, bg="#CCC", highlightthickness=0, font=("Times", 20, "bold"))
+
+		self.place(x=WIDTH/2, y=HEIGHT/2)
+		self.state = DISABLED
+
+		self.duration = duration
+		self.remainder = 0
+		self.start_time = 0
+
+	def start(self):
+		self.start_time = time()
+
+	def update(self):
+		self.calulateTime()
+
+		self.state = NORMAL
+		self.insert(INSERT, self.remainder)
+		self.state = DISABLED
+
+	def calulateTime(self):
+		self.remainder = self.duration - (time() - self.start_time)
 
 #----[MAIN]----------------------------------------------------------
 # Setup window, originally 800x480
 WIDTH = 800
 HEIGHT = 480
+alive = True
 
 window = Tk()
 window.overrideredirect(1)
@@ -165,7 +213,11 @@ window.geometry("{}x{}".format(WIDTH, HEIGHT))
 g = Game(window)
 g.play()
 
-window.mainloop()
+while(alive):
+	g.handleEvents()
+	window.update_idletasks()
+	window.update()
+	sleep(0.01)
 
 # DIFFICULTIES:
 # - Control-Alt-Deletus (Easy) - 
