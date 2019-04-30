3810cc06b095b0026bb9a30a4438218f4f0f9466
Ashton Scott Snapp <ashton@ashtons.xyz>
2019-04-30 17:38:48 -0500

    Added main.py and modified hardware.py
    Created main.py, added imports for the other source files, created closeGame() to close the game, and added an empty sequence list to the end of hardware.py

--- src/main.py
+++ src/main.py
@@ -8,7 +8,15 @@
 from hardware import hardware as hw
 from graphics import gui
 from logic import rhClasses as rohe
+from random import randint
 
-io = hw.HardIO()
+# A function to be called that safely closes everything down. Takes in a reference to a HardIO object.
+def closeGame(hard_io):
+	# Closing remarks to be printed when the game closes. Feel free to add your own strings!
+	closingRemarks = ["Have an awesome day!", "May the force be with you.", "Thank you for playing!", "Exited with status code 0 - success!", "b a n a n a"]
 
-# And then you continue.
+	hard_io.destroy()
+
+	print closingRemarks[randint(0, len(closingRemarks) - 1)]
+
+	exit(0)
