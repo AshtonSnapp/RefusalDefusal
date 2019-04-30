75ea2c4ba017ba5fe2a25f1373a85174f0cafe06
Ashton Scott Snapp <ashton@ashtons.xyz>
2019-04-30 17:07:28 -0500

    whoops
    Just found out that I could just do this. Nevermind on the __init__ files!

---
+++ src/main.py
@@ -0,0 +1,14 @@
+#####################################################################
+# Name: Carrick Inabnett, Ashton Snapp, Justin Crouch
+# Last Update: 30 April 2019
+# Changes: Main
+#####################################################################
+
+
+from hardware import hardware as hw
+from graphics import gui
+from logic import rhClasses as rohe
+
+io = hw.HardIO()
+
+# And then you continue.
