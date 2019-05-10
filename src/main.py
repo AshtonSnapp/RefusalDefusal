34a05935e4ef32d97b6f92d8b271e3a9fffcc599
raccoonbroVA <49129756+raccoonbroVA@users.noreply.github.com>
2019-05-10 14:46:20 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -33,18 +33,38 @@ gui, window = GUI.setup()
 # List of easy mode sequences
 # Each sequence has an activity and a hint for that activity
 easy_sequences = [["f100", "p100", "e413", "f000"], 
-		  ["f010", "p010", "e2", "p000", "f000"], 
+		  ["f010", "p010", "e2", "f000"], 
 		  ["p011", "e24", "f101", "p001"], 
 		  ["p110", "e41", "p000", "e231"]]
 
-easy_hints = [["Flip switch 1", "Pull green and blue", "Enter 413", "Flip switch 1"],
-              ["Flip switch 2", "Pull red and and blue", "Enter 2", "Pull green", "Flip switch 2"],
-              ["Pull red", "Enter 24", "Flip Switch 1 and 3", "Pull green"],
-              ["Pull blue", "Enter 41", "Pull red and green", "Enter 231"]]
+easy_hints = [["It's lights out for this robot!", "Just line 'em up and knock 'em down!", "I metaphor once. He wanted to be a 3.", "How many people does it take to screw in a light bulb? Two! One to"\
+               "screw it in, and another to hold the chair steady... did I say it right?"],
+              ["How many people does it take to screw in a light bulb? Two! One to screw it in, and another to hold the chair steady... did I say it right?",\
+               "Try pulling her finger. You never know what might happen.", "I'll help you. I just hope I'm not pressing your buttons champ.", "It's lights out for this robot!"],
+              ["Just line 'em up and knock 'em down!", "I metaphor once. He wanted to be a 3.", "It's lights out for this robot!", "Try pulling her finger. You never know what might happen."],
+              ["Try pulling her finger. You never know what might happen.", "I metaphor once. He wanted to be a 3.", "Just line 'em up and knock 'em down!",\
+               "I'll help you. I just hope I'm not pressing your buttons champ."]]
+
+medium_sequences = [["e42", "p100", "f100", "f001", "e31"],
+                    ["f101", "p010", "e42143", "p000", "f000"],
+                    ["f110", "e12", "p110", "e324", "f011"],
+                    ["e34", "p100", "f110", "p000", "e412"]]
+
+medium_hints = [["Don't fret over a lawsuit. You can leave those pressing matters to me. Having said that, please don't fail.", "My hair will likely disintegrate if I haven't already pulled it out.",\
+                 "I knew I should've switched jobs last week.", "My boss will flip when he hears this!", "According to my calcula- WAIT, where's my calculator?!"],
+                ["I knew I should've switched jobs last week.", "My hair will likely disintegrate if I haven't already pulled it out.", "Don't fret over a lawsuit. You can leave those pressing matters to me. Having said that, please don't fail.",\
+                 "I really hope this doesn't affect the city's power lines.", "I knew I should've switched jobs last week."],
+                ["I knew I should've switched jobs last week.", "According to my calcula- WAIT, where's my calculator?!", "My hair will likely disintegrate if I haven't already pulled it out.",\
+                 "Don't fret over a lawsuit. You can leave those pressing matters to me. Having said that, please don't fail.", "My boss will flip when he hears this!"],
+                ["Don't fret over a lawsuit. You can leave those pressing matters to me. Having said that, please don't fail.", "I really hope this doesn't affect the city's power lines.",\
+                 "I knew I should've switched jobs last week.", "My hair will likely disintegrate if I haven't already pulled it out.", "According to my calcula- WAIT, where's my calculator?!"]]
+
+hard_sequences = 
 
 sequence_hints = []
 sequence = []
-completed = 0
+completed = False
+#gameOver = 0 # 0: stop, 1: play, 2: win, 3: lose
 #----[MAIN]----------------------------------------------------------
 # Start the game
 gui.play()
@@ -61,7 +81,15 @@ while(gui.running):
         # Generate an easy sequence if not already generated
         if(len(sequence) == 0):
             sequence = choice(easy_sequences)
-            sequence_hints = easy_hints[ easy_sequences.index(sequence) ]
+            sequence_hints = easy_hints[easy_sequences.index(sequence)]
+
+    if(gui.difficulty == "Medium"):
+
+        gui.gameOver = 1
+
+        if(len(sequence) == 0):
+            sequence = choice(medium_sequences)
+            sequence_hints = medium_hints[medium_sequences.index(sequence)]
 
     # If user not on a game difficulty, unload sequence
     if(gui.difficulty == "None"):
@@ -81,18 +109,6 @@ while(gui.running):
         del sequence_hints[0]
         del sequence[0]
         completed = False
-
-    # If completed == 0 (Not Complete), light LED blue
-    if(completed == 0):
-    	io.RGB("001")
-
-    # If completed == 1 (Failed), light LED red
-    if(completed == 1):
-    	io.RGB("100")
-
-    # If completed == 2 (Success), light LED green
-    if(completed == 1):
-    	io.RGB("010")
 
     if(gui.game_over == 1):
         if(len(sequence) == 0):
